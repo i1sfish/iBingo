2b8163b0bed995533b482303b9de9ffdb0144497
yupeng <i1.sfish@foxmail.com>
2017-12-23 10:14:49 +0800

    test

--- src/main/java/com/ibingo/controller/HelloController.java
+++ src/main/java/com/ibingo/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @RequestMapping("/helloWorld")
     public String helloWorld(Model model){
-        model.addAttribute("message","Hello World!");
+        model.addAttribute("message","你好，世界！");
         return "hello";
     }
 }
