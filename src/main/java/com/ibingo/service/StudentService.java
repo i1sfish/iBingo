bd1c85cc642c33403bf228dfc9d9fa3ccb6b47cd
yupeng <i1.sfish@foxmail.com>
2017-12-23 15:15:46 +0800

    test

--- src/main/java/com/ibingo/service/StudentService.java
+++ src/main/java/com/ibingo/service/StudentService.java
@@ -4,12 +4,11 @@ import com.ibingo.dao.StudentDao;
 import com.ibingo.model.Student;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Service("studentService")
 public class StudentService {
-    @Resource(name = "studentDao")
+
     private StudentDao dao;
 
     public List<Student> getStudentList() {
