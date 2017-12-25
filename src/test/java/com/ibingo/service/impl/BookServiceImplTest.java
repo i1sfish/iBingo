ddde876ed8719059a0e0af547e7da0390d5bbdd5
yupeng <i1.sfish@foxmail.com>
2017-12-25 19:56:25 +0800

    test

---
+++ src/test/java/com/ibingo/service/impl/BookServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.ibingo.service.impl;
+
+import com.ibingo.BaseTest;
+import com.ibingo.dto.AppointExecution;
+import com.ibingo.service.BookService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BookServiceImplTest extends BaseTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    public void testAppoint() throws Exception {
+        long bookId = 1001;
+        long studentId = 12345678910L;
+        AppointExecution execution = bookService.appoint(bookId, studentId);
+        System.out.println(execution);
+    }
+
+}
